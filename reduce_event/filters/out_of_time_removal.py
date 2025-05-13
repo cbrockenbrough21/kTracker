85072737bca049cb45d26598b8a1b7dea9cc5a7f
cbrockenbrough21 <cqt5de@virginia.edu>
2025-05-13 13:36:27 -0400

    Added timing to reduce_event and cleaned

--- reduce_event/filters/out_of_time_removal.py
+++ reduce_event/filters/out_of_time_removal.py
@@ -4,18 +4,17 @@ Out-of-Time Hit Removal Filter
 
 import numpy as np
 
-def remove_out_of_time_hits(tdc_times, tdc_center, tdc_width, keep_idx):
+def remove_out_of_time_hits(tdc_times, keep_idx):
     """
     Filters hits based on TDC center and width.
     Args:
-        tdc_times (np.array): Full array of TDC times
-        tdc_center (float): Center of the allowed window
-        tdc_width (float): Full width
+        tdc_times (list): Full array of TDC times
         keep_idx (list): List of indices to check and filter
     Returns:
         list: Updated keep_idx
     """
-    lower = tdc_center - (tdc_width / 2)
-    upper = tdc_center + (tdc_width / 2)
+    # lower = tdc_center - (tdc_width / 2)
+    # upper = tdc_center + (tdc_width / 2)
 
-    return [i for i in keep_idx if lower <= tdc_times[i] <= upper]
+    # return [i for i in keep_idx if lower <= tdc_times[i] <= upper]
+    return keep_idx
